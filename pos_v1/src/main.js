4218f3a8f7d40f8177486498561ba0124b5f6cb0
SanCoder-Q <sancoder.q@gmail.com>
2014-10-28 11:43:54 +0800

    Simply optimizing four time traversal to one.
    SanCoder 2014-10-28
    
    printInventory实现
    根据购物商品的条形码数组实现打印POS机小票的功能。
    
    要提交的变更：
    修改：     pos_v1/src/main.js
    
    修改：
    1.getShoppingList函数为获取购物车内商品的种类和每个种类的数量，参数为商品数组和条形码输入，由内部逻辑解析条形码数组，并在返回的数组的对象元素item中增加price和discountPrice属性;
    2.printInventory函数由4次遍历减少至1次，在循环中直接计算商品价格和折扣，并统计总价格和总折扣;
    3.修复selectObjectInArray函数返回obj对象的bug。
    
    备注：
    简单的代码优化。

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,92 +1,88 @@
 //SanCoder 2014-10-27
 //TW homework: Pos_v1
 
-//Get a initial item list include count and whether is discounting.
-function getInitialShoppingList(itemArray)
-{
-  var shopingList = new Array();
-  for(var index in itemArray) {
-    var item = itemArray[index];
-    shopingList[item.barcode] = {'itemInfo':item,
-      'count':0,
-      'discount':0,
-      'isDiscount':function(discountItemArray) {
-          for(var i in discountItemArray){
-            if(discountItemArray[i] == this.itemInfo.barcode){
-              return true;
-            }
-          }
-          return false;
-        }
-      };
-  }
-  return shopingList;
-}
-
 //Select the object from an array
 function selectObjectInArray(objectArray, key, value) {
   for(var index in objectArray)
     if(objectArray[index][key] == value)
-      return obj;
+      return objectArray[index];
   return null;
 }
+
+//Get a shopping list with the quantity purchased.
+function getShoppingList(itemArray, barcodeList)
+{
+  var shopingList = new Array();
+  for(var index in barcodeList) {
+    var strArray = barcodeList[index].split('-');
+    var item = selectObjectInArray(itemArray, 'barcode', strArray[0]);
+    var count = strArray.length > 1 ? parseInt(strArray[1], 10) : 1;
+    if(typeof(shopingList[item.barcode]) == "undefined") {
+      shopingList[item.barcode] = {'itemInfo':item,
+        'count':count,
+        'discount':0,
+        'price':0,
+        'discountPrice':0,
+        'isDiscount':function(discountItemArray) {
+            for(var i in discountItemArray){
+              if(discountItemArray[i] == this.itemInfo.barcode){
+                return true;
+              }
+            }
+            return false;
+          }
+        };
+      }
+      else {
+        shopingList[item.barcode].count += count;
+      }
+  }
+  return shopingList;
+}
+
+
 
 //output
 function printInventory(barcodeList) {
   //parameter validation
+
+
+  //initial output string
   var outputStr = '';
-  var shoppingList = getInitialShoppingList(loadAllItems());
-  var discountItemArray;
+
+  //initial shoppingList
+  var shoppingList = getShoppingList(loadAllItems(), barcodeList);
+
+  //initial discountList
+  var discountList;
   var blocks = loadPromotions();
   for(var i in blocks) {
     if(blocks[i].type == 'BUY_TWO_GET_ONE_FREE') {
-      discountItemArray = blocks[i].barcodes;
+      discountList = blocks[i].barcodes;
       break;
     }
   }
 
   outputStr += '***<没钱赚商店>购物清单***\n';
+
   //goods traversal, statistics count
-  for(var i in barcodeList) {
-    var barcodeStr = barcodeList[i];
-    var strArray = barcodeStr.split('-');
-
-    var barcodeStr = strArray[0];
-    var item = shoppingList[barcodeStr];
-    var num = strArray.length > 1 ? parseInt(strArray[1], 10) : 1;
-
-    if(item == null)
-      throw "Cannot find the item by barcode.";
-    if(num <= 0)
-      throw "Wrong item number.";
-
-    item.count += num;
-  }
-
   var sumPrice = 0;
-  //count price
+  var discountPrice = 0;
+  var discountStr = '----------------------\n挥泪赠送商品：\n';
   for(var i in shoppingList) {
     var item = shoppingList[i];
-    if(item.count <= 0)  continue;
-
-    var price;
-    if(item.isDiscount(discountItemArray))
+    if(item.isDiscount(discountList))
       item.discount = Math.floor(item.count / 3);
-
-    price = (item.count - item.discount) * item.itemInfo.price;
-    sumPrice += price;
-    outputStr += '名称：' + item.itemInfo.name + '，数量：' + item.count + item.itemInfo.unit + '，单价：' + item.itemInfo.price.toFixed(2) + '(元)，小计：' + price.toFixed(2) + '(元)\n';
+    item.discountPrice = item.discount * item.itemInfo.price;
+    item.price = item.count * item.itemInfo.price - item.discountPrice;
+    sumPrice += item.price;
+    discountPrice += item.discountPrice;
+    outputStr += '名称：' + item.itemInfo.name + '，数量：' + item.count + item.itemInfo.unit + '，单价：' + item.itemInfo.price.toFixed(2) + '(元)，小计：' + item.price.toFixed(2) + '(元)\n';
+    if(item.discount > 0)
+      discountStr += '名称：' + item.itemInfo.name + '，数量：' + item.discount + item.itemInfo.unit + '\n';
   }
 
-  outputStr += '----------------------\n挥泪赠送商品：\n';
-  var discountPrice = 0;
-  for(var i in discountItemArray) {
-    item = shoppingList[discountItemArray[i]];
-    if(item.discount <= 0)
-      continue;
-    discountPrice += item.discount * item.itemInfo.price;
-    outputStr += '名称：' + item.itemInfo.name + '，数量：' + item.discount + item.itemInfo.unit + '\n';
-  }
+  outputStr += discountStr;
 
   outputStr += '----------------------\n';
   outputStr += '总计：' + sumPrice.toFixed(2) + '(元)\n';
