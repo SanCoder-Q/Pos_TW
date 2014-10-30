a708ff4ba3a6d57faf04ebaba19d4434540e2947
SanCoder-Q <sancoder.q@gmail.com>
2014-10-30 15:53:09 +0800

    Change the price count logic to Mall.PromotionManager.
    SanCoder 2014-10-30
    
    printInventory实现
    根据购物商品的条形码数组实现打印POS机小票的功能。
    
    要提交的变更：
    修改：     pos_v1/src/main.js
    
    修改：
    1.将selectObjectInArray方法由ShoppingCart移至Array中作为静态函数;
    2.增加Util.Validate.classValidate验证对象类型;
    3.移动validateItemBarcode方法验证商品扫描码存在至Mall.ItemManager验证商品扫描码存在;
    4.增加Mall.PromotionManager.getPromotionListofAnItem方法获取单个商品的所有促销列表;
    5.将countPrice逻辑由ShoppingCart移动到Mall.PromotionManager中;
    6.增加ShoppingCart中_sumPrice和_sumDiscout字段的Setter方法。

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -12,7 +12,30 @@ Object.clone = function(obj) {
     objClone[i] = Object.clone(obj[i]);
   }
   return objClone;
-}
+};
+
+//Select the object from an array.
+//if parameters without key or key == null,
+//this function treats the objectArray as an array,
+//otherwise it treats the objectArray as an dictionary.
+Array.selectObjectInArray = function(objectArray, value, key) {
+  //parameter validation
+  Util.Validate.paraNumValidate(arguments, 2, 3);
+  Util.Validate.nullValidate(objectArray, value);
+
+  //overload the method
+  if(arguments.length == 2 || key == null) {
+    for(var i in objectArray)
+      if(objectArray[i] == value)
+        return objectArray[i];
+  }
+  else{
+    for(var i in objectArray)
+      if(objectArray[i][key] == value)
+        return objectArray[i];
+  }
+  return null;
+};
 
 var Util = {};
 
@@ -53,10 +76,16 @@ Util.Validate = (function(){
       if(args.length < minNum || args.length > maxNum)
         throw "ParaNumValidate: Parameter number error in function: " + /function\s+(\w+)/.exec(arguments.callee.caller)[1];
     },
+    //validate the class of the object
+    classValidate: function(obj, expectClass) {
+      this.paraNumValidate(arguments, 2);
+      this.nullValidate(arguments);
+      if(!(obj instanceof expectClass))
+        throw "ClassValidate: The object is not an instance of the expect class in funciton: " + /function\s+(\w+)/.exec(arguments.callee.caller)[1];
+    },
   };
   return publicReturn;
 })();
-
 
 var Mall = {};
 
@@ -69,6 +98,14 @@ Mall.ItemManager = (function(){
     getItemList: function(){
       return Object.clone(_itemList);
     },
+    //validate whether the barcode exists
+    validateItemBarcode: function(barcodeStr) {
+      //parameter validation
+      Util.Validate.paraNumValidate(arguments, 1);
+      Util.Validate.nullValidate(arguments);
+
+      return null == Array.selectObjectInArray(_itemList, barcodeStr, 'barcode');
+    },
   };
   return publicReturn;
 })();
@@ -79,7 +116,7 @@ Mall.PromotionManager = (function(){
   var _promotions = loadPromotions();
   var publicReturn = {
     //#public method:
-    getItemListOfAPromotion:function(promotionTypeStr) {
+    getItemListOfAPromotion: function(promotionTypeStr) {
       //parameter validation
       Util.Validate.paraNumValidate(arguments, 1);
       Util.Validate.nullValidate(arguments);
@@ -91,6 +128,46 @@ Mall.PromotionManager = (function(){
       }
 
       throw "Wrong promotion type: " + promotionTypeStr;
+    },
+
+    getPromotionListofAnItem: function(itemBarcodeStr) {
+      //parameter validation
+      Util.Validate.paraNumValidate(arguments, 1);
+      Util.Validate.nullValidate(arguments);
+      if(!Mall.ItemManager.validateItemBarcode(itemBarcodeStr))
+        return null;
+
+      var promotionList = [];
+      for(var i in _promotions) {
+        if(Array.selectObjectInArray(_promotions[i].barcodes, itemBarcodeStr != null)) {
+          promotionList.push(_promotions[i].type);
+        }
+      }
+
+      return promotionList;
+    },
+
+    //Counting every item price and discount
+    countPrice: function(shoppingCart) {
+      //parameter validation
+      Util.Validate.paraNumValidate(arguments, 1);
+      Util.Validate.nullValidate(arguments);
+      Util.Validate.classValidate(shoppingCart, ShoppingCart);
+
+      var shoppingList = shoppingCart.getShoppingList();
+      var sumPrice = 0;
+      var sumDiscount = 0;
+      for(var i in shoppingList) {
+        var item = shoppingList[i];
+        if(item.isDiscount)
+          item.discount = Math.floor(item.count / 3);
+        item.discountPrice = item.discount * item.itemInfo.price;
+        item.price = item.count * item.itemInfo.price - item.discountPrice;
+        sumPrice += item.price;
+        sumDiscount += item.discountPrice;
+      }
+      shoppingCart.setSumPrice(sumPrice);
+      shoppingCart.setSumDiscount(sumDiscount);
     },
   };
   return publicReturn;
@@ -112,14 +189,14 @@ function ShoppingCart(barcodeList, discountItemArray) {
   //#public method:
   this.getShoppingList = function(){ return _shoppingList; }; //it seem that there isn't a way to define a prototype method which can access the private member in JS. what a pity!
   this.getSumPrice = function(){ return formatMoney(_sumPrice); };
+  this.setSumPrice = function(value){
+    Util.Validate.nullValidate(value);
+    _sumPrice = value;
+  };
   this.getSumDiscount = function(){ return formatMoney(_sumDiscount); };
-  //validate whether the barcode exists
-  this.validateItemBarcode = function(barcodeStr) {
-    //parameter validation
-    Util.Validate.paraNumValidate(arguments, 1);
-    Util.Validate.nullValidate(arguments);
-
-    return null == selectObjectInArray(_itemList, barcodeStr, 'barcode');
+  this.setSumDiscount = function(value){
+    Util.Validate.nullValidate(value);
+    _sumDiscount = value;
   };
 
   //#private method:
@@ -132,7 +209,7 @@ function ShoppingCart(barcodeList, discountItemArray) {
     //Initial the shoppingList
     for(var i in barcodeList) {
       var strArray = barcodeList[i].split('-');
-      var item = selectObjectInArray(_itemList, strArray[0], 'barcode');
+      var item = Array.selectObjectInArray(_itemList, strArray[0], 'barcode');
       var count = strArray.length > 1 ? parseInt(strArray[1], 10) : 1;
       if(typeof(_shoppingList[item.barcode]) == "undefined") {
         _shoppingList[item.barcode] = {'itemInfo':item,
@@ -140,7 +217,7 @@ function ShoppingCart(barcodeList, discountItemArray) {
           'discount':0,
           'price':0,
           'discountPrice':0,
-          'isDiscount':selectObjectInArray(discountItemArray, item.barcode) != null
+          'isDiscount':Array.selectObjectInArray(discountItemArray, item.barcode) != null
         };
       }
       else {
@@ -149,45 +226,9 @@ function ShoppingCart(barcodeList, discountItemArray) {
     }
   }
 
-  //Counting every item price and discount
-  function countPrice() {
-    for(var i in _shoppingList) {
-      var item = _shoppingList[i];
-      if(item.isDiscount)
-        item.discount = Math.floor(item.count / 3);
-      item.discountPrice = item.discount * item.itemInfo.price;
-      item.price = item.count * item.itemInfo.price - item.discountPrice;
-      _sumPrice += item.price;
-      _sumDiscount += item.discountPrice;
-    }
-  }
-
-  //Select the object from an array.
-  //if parameters without key or key == null,
-  //this function treats the objectArray as an array,
-  //otherwise it treats the objectArray as an dictionary.
-  function selectObjectInArray(objectArray, value, key) {
-    //parameter validation
-    Util.Validate.paraNumValidate(arguments, 2, 3);
-    Util.Validate.nullValidate(objectArray, value);
-
-    //overload the method
-    if(arguments.length == 2 || key == null) {
-      for(var i in objectArray)
-        if(objectArray[i] == value)
-          return objectArray[i];
-    }
-    else{
-      for(var i in objectArray)
-        if(objectArray[i][key] == value)
-          return objectArray[i];
-    }
-    return null;
-  }
-
   //#constructor logic:
   initialize();
-  countPrice();
+  Mall.PromotionManager.countPrice(this);
 }
 
 //output
